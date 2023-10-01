cb5f481abd8d263f0884d54dd442829c08645c95
candrews42 <andrews.colinj@gmail.com>
2023-10-01 19:30:22 +0300

    working

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ class generative_agriculture:
                 sql_response = chatbot_agent.run(chatbot_response, callbacks=[st_cb])
                 # sql_response = sql_agent.run(chatbot_response, callbacks=[st_cb])
                 st.session_state.messages.append({"role": "assistant", "content": sql_response})
-                st.write(sql_response)
+                # st.write(sql_response)
                 # else:
                 #    chatbot_response = chatbot_agent.run(user_query, callbacks=[st_cb])
                 #    st.session_state.messages.append({"role": "chatbot", "content": chatbot_response})
