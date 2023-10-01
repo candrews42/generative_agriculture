2e543706eb4454a2421ec522167132218caa7138
candrews42 <andrews.colinj@gmail.com>
2023-10-01 19:53:42 +0300

    trying pages

---
+++ pages/about.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="About", page_icon="🌱", layout="wide")
+st.header("Colin Andrews")
+st.write("email: colin@generativeagriculture.com, IG: perma.colin")
