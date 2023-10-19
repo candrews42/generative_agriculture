0f6545a931d91cf07cc1ac078e49ab71e0c575e6
candrews42 <andrews.colinj@gmail.com>
2023-10-19 12:43:42 +0300

    updated about page

--- pages/about.py
+++ pages/about.py
@@ -2,3 +2,70 @@ import streamlit as st
 st.set_page_config(page_title="About", page_icon="🌱", layout="wide")
 st.header("Colin Andrews")
 st.write("email: colin@generativeagriculture.com, IG: perma.colin")
+
+# Introduction
+st.header("Introduction")
+st.write("""
+My name is Colin Andrews. I currently work at Learn to Grow, an outdoor educational center and regenerative farm.
+With a background in strategy consulting, systems engineering, and AI integration, I now focus on applying modern 
+systems thinking to natural processes. My work aims to use emerging AI tools and modern systems design methods to address several UN sustainable development goals and contribute 
+to the ecology of Bahrain and the Gulf region.
+""")
+
+# Professional Background
+st.header("Professional Background")
+st.write("""
+- **Master's in Computer Science**: Focused on machine learning and artificial intelligence.
+- **Bachelor's in Electrical Engineering**
+- **Strategy Consulting**: End-to-end supply chain analytics and optimization.
+- **Blockchain & Energy Markets**: Designed a multi-attribute double-sided auction for local energy communities.
+""")
+
+# Experience
+st.header("Experience")
+st.write("""
+**Learn to Grow, Bahrain**
+- Farm and Business Manager
+- Permaculture Design Consultant
+- [Learn to Grow Instagram](https://instagram.com/learntogrow.bh?igshid=MzRlODBiNWFlZA==)
+
+**Grid Singularity, Lisbon & Berlin**
+- Research Scientist in energy market design and decentralization
+
+**Freeel.io, Vienna**
+- Incentive Design for Clean Energy Ecosystems
+
+**Kurt Salmon, part of Accenture Strategy, New York & Atlanta, USA**
+- Business and Analytics Strategy Consultant
+""")
+
+# Articles & Contributions
+st.header("Articles & Contributions")
+st.write("""
+- [Discussion Paper: Grid Singularity's Implementation of Symbiotic Energy Markets](https://gridsingularity.medium.com/discussion-paper-grid-singularitys-implementation-of-symbiotic-energy-markets-bd3954af43c8)
+- [Fractal Ownership: A Token Engineering Concept](https://medium.com/p/54e7edc58953)
+""")
+
+# Skills & Technologies
+st.header("Skills & Technologies")
+st.write("""
+- **Programming**: Python, API Integration (ChatGPT, Langchain, MongoDB, etc.)
+- **AI/ML Libraries**: Tensorflow, Sci-kit learn
+- **Product-Building Skills**: Prototyping, Requirements Engineering, Systems Design, Presentation, Team Leadership, Analytical Thinking  
+- **Agri Skills**: Basic carpentry, basic electronics, composting and soil building, farm management, and permaculture techniques
+- **Languages**: English (native), German (proficient), Portuguese & Spanish (functional), Arabic (beginner)
+""")
+
+# Contact & Social Media
+st.header("Contact & Social Media")
+st.write("""
+- 📧 Email: [andrews.colinj@gmail.com](mailto:andrews.colinj@gmail.com)
+- 🌐 [LinkedIn](https://linkedin.com/in/andrews.co)
+- 🐱 [GitHub](#)  # Add your GitHub URL here
+- 📸 [Instagram: Learn to Grow](https://instagram.com/learntogrow.bh?igshid=MzRlODBiNWFlZA==)
+""")
+
+# Resume
+st.header("Resume")
+st.write("You can download my resume [here](#).")  # Add a link to your resume here
+
