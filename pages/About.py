bb5a118acdb76c040e39d331af387a6541dca251
candrews42 <andrews.colinj@gmail.com>
2023-10-22 14:15:47 +0300

    updated about

---
+++ pages/About.py
@@ -0,0 +1,66 @@
+import streamlit as st
+
+# Set up the layout and title
+st.set_page_config(page_title="About", page_icon="🌱", layout="wide")
+
+# Create columns for layout
+col1, col2 = st.columns([1, 3])
+
+# Column 1: Display the profile image
+profile_image_url = "https://media.licdn.com/dms/image/D4D03AQEGjcJmBU8JGw/profile-displayphoto-shrink_800_800/0/1695462094237?e=1703116800&v=beta&t=hOlAfX3j-JZZzl4sIfEgsq6R_vaaPME5TjfiS3wPYmw"
+col1.image(profile_image_url, caption="Colin Andrews", width=300)
+
+# Introduction
+st.write("""
+**Colin Andrews**  is the farm manager at Learn to Grow Outdoor Educational Center and Regenerative Farm, applying modern systems thinking to natural processes to build methods that directly address several UN sustainable development goals and are immediately useful to regenerating the ecology of Bahrain and the Gulf region.
+
+Colin has had a diverse career in **strategy consulting and analytics for global end-to-end supply chain, systems engineering for blockchain-based local energy communities**, and the **integration of artificial intelligence algorithms into energy market trading**.
+""")
+
+# Professional Background
+st.header("Education")
+st.write("""
+**Georgia Institute of Technology**
+- **Master's in Computer Science**: Focused on machine learning and artificial intelligence.
+- **Bachelor's in Electrical Engineering**
+- **Minor Engineering & Business Management** through Denning Technology & Management Program \n
+**Oregon State University**: Permaculture Design Consultant Professional Certification
+""")
+
+# Experience
+st.header("Work Experience")
+st.write("""
+**[Learn to Grow](https://instagram.com/learntogrow.bh?igshid=MzRlODBiNWFlZA==)** :: Regenerative Farm Manager and Permaculture Design Consultant :: **Bahrain 2023**
+
+**[Grid Singularity](gridsingularity.com)** :: Research Scientist in energy market design and decentralization. Designed and implemented a multi-attribute double-sided auction for local energy communities. :: **Berlin & Lisbon 2019-2022**
+
+**[Freeel.io](https://freeel.io/)** :: Incentive Design and token engineering for Clean Energy Ecosystems. ::**Vienna 2019**
+
+**[Kurt Salmon, part of Accenture Strategy](https://www.accenture.com/us-en/about/strategy-index)** :: Business and Analytics Strategy Consultant for end-to-end supply chain analytics and optimization. :: **New York & Atlanta, USA 2015-2018**
+""")
+
+# Articles & Contributions
+st.header("Articles & Contributions")
+st.write("""
+- [Discussion Paper: Grid Singularity's Implementation of Symbiotic Energy Markets](https://gridsingularity.medium.com/discussion-paper-grid-singularitys-implementation-of-symbiotic-energy-markets-bd3954af43c8)
+- [Fractal Ownership: A Token Engineering Concept](https://medium.com/p/54e7edc58953)
+""")
+
+# Skills & Technologies
+st.header("Skills & Technologies")
+st.write("""
+- **Programming**: Python, API Integration (ChatGPT, Langchain, MongoDB, etc.), Streamlit, AI and ML Libraries (Tensorflow, Sci-kit learn), basic AWS and Google Cloud integration, SQL and PostgreSQL
+- **Product-Building Skills**: Prototyping, Requirements Engineering, Systems Design, Presentation, Team Leadership, Analytical Thinking  
+- **Agri Skills**: Basic carpentry, basic electronics, composting and soil building, farm management, and permaculture techniques
+- **Languages**: English (native), German (proficient), Portuguese & Spanish (functional), Arabic (beginner)
+""")
+
+# Contact & Social Media
+st.header("Contact & Social Media")
+st.write("""
+- 📧 Email: [colin@generativeagriculture.com](mailto:colin@generativeagriculture.com)
+- 🌐 [LinkedIn](https://linkedin.com/in/andrews.co)
+- 🐱 [GitHub](https://github.com/candrews42) 
+- 📸 [Instagram: Learn to Grow](https://instagram.com/learntogrow.bh?igshid=MzRlODBiNWFlZA==)
+""")
+
